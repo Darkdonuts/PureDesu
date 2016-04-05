e0781c644aa97a7b6a0c26fecba26810677318f0
Rori <darkudonutsu@gmail.com>
2016-04-06 01:24:31 +0200

    added 1st TODO
    I'm not sure if putting it on pured.cpp is the correct way to do it but,
        that's how I've been doing it lately.

--- pured.cpp
+++ pured.cpp
@@ -1,5 +1,10 @@
 /*
  * My personal assistant waifu
+ *
+ * TODO: Able to create a file containing a list of interesting animu that are airing
+ *       If the list file is empty, ask the user to configure the list for the first
+ *       time. Each element in the list should have: date start, date end, airing
+ *       hour, and name (for now romaji only).
  */
 #include "pured_utils.h"
 
