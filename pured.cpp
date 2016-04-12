39ae0806de2232bb59f5c0e09f2c2e95712a3961
Rori <darkudonutsu@gmail.com>
2016-04-12 13:58:24 +0200

    added TODO
    closes #1

--- pured.cpp
+++ pured.cpp
@@ -1,10 +1,6 @@
 //////////////////////////////////////////////////////////////////////////////////////
-// My personal assistant waifu
 //
-// TODO: Able to create a file containing a list of interesting animu that are airing
-//       If the list file is empty, ask the user to configure the list for the first
-//       time. Each element in the list should have: date start, date end, airing
-//       hour, and name (for now romaji only).
+// My personal assistant waifu
 //
 /////////////////////////////////////////////////////////////////////////////////////
 #include "pured_utils.h"
